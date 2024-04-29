241c44dd5d5c0681b5021c0f41b36ac69cef89e6
SumantK <sumant.kantamsetty@gmail.com>
2024-04-29 22:06:40 +0530

    NavBar Pointer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
           ))}
         </div>
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="bag" width={18} height={18} />
+          <img src={searchImg} alt="search" width={18} height={18} className="cursor-pointer"/>
+          <img src={bagImg} alt="bag" width={18} height={18} className="cursor-pointer"/>
         </div>
       </nav>
     </header>
